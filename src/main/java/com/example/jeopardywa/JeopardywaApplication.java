7b39e60dd3da1dc50e3ed7a0d1bfbbb444655970
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-02 23:39:15 -0600

    Add connection to JeopardyWebApp db

--- src/main/java/com/example/jeopardywa/JeopardywaApplication.java
+++ src/main/java/com/example/jeopardywa/JeopardywaApplication.java
@@ -2,12 +2,27 @@ package com.example.jeopardywa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@Controller
 public class JeopardywaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JeopardywaApplication.class, args);
+	@RequestMapping("/")
+	public String readIndex() {
+		String getIndex = getIndex();
+		return getIndex();
 	}
+	
 
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(JeopardywaApplication.class, args);
+	// }
+
+	private String getIndex() {
+		return "/src/main/resources/frontend/index.html";
+	}
 }
