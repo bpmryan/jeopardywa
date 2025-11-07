3024230d4b0994a4e6e5edd8e4b4c6c360dd92d9
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-06 22:25:15 -0600

    Comment out iframes out for demo

--- src/main/java/JeopardyApp.java
+++ src/main/java/JeopardyApp.java
@@ -1,10 +1,14 @@
 // package jeopardywa.src.main.java;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JeopardyApp {
+	public static void main(String[] args) {
+		SpringApplication.run(JeopardyApp.class, args);
+	}
     @RequestMapping("/")
 	public String index() {
 		return "index.html";
--- src/main/java/com/example/jeopardywa/JeopardywaApplication.java
+++ src/main/java/com/example/jeopardywa/JeopardywaApplication.java
@@ -1,24 +1,15 @@
 package com.example.jeopardywa;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-// Doesn't work at the moment
-@SpringBootApplication
 @Controller
 public class JeopardywaApplication {
-	@RequestMapping("/")
-	public String readIndex() {
-		
+	public static void main(String[] args) {
+		SpringApplication.run(JeopardywaApplication.class, args);
+	}
+    @RequestMapping("/")
+	public String index() {
 		return "index.html";
 	}
-	
-
-	// public static void main(String[] args) {
-	// 	SpringApplication.run(JeopardywaApplication.class, args);
-	// }
 }
