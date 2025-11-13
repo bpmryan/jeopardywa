54d204e85327904c03ba11c13409b102681924b2
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-13 16:02:00 -0600

    Edit gameCreate.js and relating gameCreate html files, not working

--- src/main/resources/static/js/gameCreate.js
+++ src/main/resources/static/js/gameCreate.js
@@ -3,27 +3,34 @@
 // document.getElementById('questionContainer').appendChild(question);
 
 // Add new category HTML function
-doucment.getElementId("addCategory").addEventListener("click", async () => {
-    const response = await fetch("/gameCreate/Category.html");
-    const html = await response.text();
-    document.getElementById("mainContainer").insertAdjacentHTML("beforeand", html);
+document.getElementById("addCategory").addEventListener("click", async () => {
+  const response = await fetch("../gameCreate/CategoryItem.html");
+  const doctype = "<!DOCTYPE html>";
+  const iframeStart = '<iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="200" src="../gameCreate/CategoryItem.html">';
+  const categoryItemHTML = await response.text();
+  const iframeEnd = "</iframe></html>";
+  const html = doctype + iframeStart + categoryItemHTML + iframeEnd;
+  console.log(html);
+
+  // Check this one (inner(html))
+  document.getElementById("mainContainer").innerHTML(html);
 });
 
 // Function that helps send the category data over to db
 // Asynec functions makes it return a promise
 async function saveCategory() {
-    const data = {
-        categoryName: document.getElementById("categoryName").value,
-        bkgColor: document.getElementId("bgkColor").value,
-        textColor: doucment.getElementId("textColor").value
-    };
+  const data = {
+    categoryName: document.getElementById("categoryName").value,
+    bkgColor: document.getElementId("bgkColor").value,
+    textColor: document.getElementId("textColor").value,
+  };
 
-    // await is the promise part of the function
-    await fetch("/api/categories", {
-        method: "POST", // specified as post request 
-        headers: {"Content-Type" : "application/json"}, //server of body is declared as json
-        body: JSON.stringify(data) // data is sent to the server
-    });
+  // await is the promise part of the function
+  await fetch("/api/categories", {
+    method: "POST", // specified as post request
+    headers: { "Content-Type": "application/json" }, //server of body is declared as json
+    body: JSON.stringify(data), // data is sent to the server
+  });
 }
 
 // Add new QnA HTML dynamically
@@ -47,22 +54,21 @@ document.addEventListener("click", async (event) => {
   }
 });
 
-
 // Function that helps send the QnA data over to db
 async function saveQnA() {
-    const data = {
-        categoryId: document.getElementById("categoryId").value,
-        pointValue: parseInt(doucment.getElementById("pointValue")).value,
-        question: doucment.getElementById("questionText").value,
-        answer: doucment.getElementById("answerText").value,
-        imageUrl: document.getElementById("imageUrl").value,
-        imagePosition: document.getElementById("imagePosition").value,
-        imageScale: document.getElementById("imageScale").value 
-    };
+  const data = {
+    categoryId: document.getElementById("categoryId").value,
+    pointValue: parseInt(doucment.getElementById("pointValue")).value,
+    question: doucment.getElementById("questionText").value,
+    answer: doucment.getElementById("answerText").value,
+    imageUrl: document.getElementById("imageUrl").value,
+    imagePosition: document.getElementById("imagePosition").value,
+    imageScale: document.getElementById("imageScale").value,
+  };
 
-    await fetch("/api/qna", {
-        method: "POST", 
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data)
-    });
+  await fetch("/api/qna", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
 }
