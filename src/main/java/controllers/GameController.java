c78b144773115d407ebb52834501593bf47e7b82
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-24 14:18:15 -0600

    Add most of java backend code for game dashboard

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.GameDTO;
 import com.example.demo.service.GameService;
+
+import model.Game;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -26,5 +35,11 @@ public class GameController {
         // Returns success response
         return ResponseEntity.ok("Save gameId= " + gameId);
     }
+
+    @GetMapping("/user/{userId}")
+    public List<Game> getGamesbyUser(@PathVariable String userId) {
+        return gameRepo.findByUserId(userId);
+    }
+    
     
 }
---
+++ src/main/java/model/Game.java
@@ -0,0 +1,45 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Game") //check the name of the table
+public class Game {
+    
+    @Id
+    private String gameId;
+
+    private String userId;
+    private String gameName;
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public Game() {}
+
+    
+
+}
