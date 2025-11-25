7faeaab57d03e2aa0bdceb69b885c6ee02d4af49
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-24 22:14:52 -0600

    Fix immport error in GameController.java

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -13,12 +13,12 @@ import com.example.demo.dto.GameDTO;
 import com.example.demo.service.GameService;
 
 import model.Game;
+import repo.GameRepo;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -29,6 +29,9 @@ public class GameController {
     
     @Autowired
     private GameService gameService;
+
+    @Autowired
+    private GameRepo gameRepo;
 
     @PostMapping("/saveAll")
     public ResponseEntity<String> saveAll (@RequestBody GameDTO game) {
@@ -42,7 +45,7 @@ public class GameController {
         return gameRepo.findByUserId(userId);
     }
     
-    @DeleteMapping("/{gameId)")
+    @DeleteMapping("/{gameId}")
     public ResponseEntity<?> deleteGame(@PathVariable String gameId) {
         gameRepo.deleteById(gameId);
         return ResponseEntity.ok("Deleted");
