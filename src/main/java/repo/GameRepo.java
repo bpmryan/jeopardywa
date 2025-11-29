adce2d970dc471ef28bff88dc9358bb9410c65ff
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-28 20:46:00 -0600

    Delete an unused line in GameRepo.java

--- src/main/java/repo/GameRepo.java
+++ src/main/java/repo/GameRepo.java
@@ -11,7 +11,4 @@ public interface GameRepo extends JpaRepository<Game, String>{
     // Converts below command into sql query
     // Load all games that is associated with that userId
     List<Game> findByUserId(String userId);
-
-    // Load singular game
-    Game findbyGameId(String gameId);
 }
