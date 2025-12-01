61e0b19670c8be903cdb4050a8b1e73ec70032e7
bpmryan <bpm_ryand2428@yahoo.com>
2025-11-30 22:54:09 -0600

    Revert Category.java back because of constructor retrieval error

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -11,19 +11,9 @@ public class Category {
     @Id
     private String categoryId;
     private String gameId;
-
-    private String qnaId;
-    private Integer ptValue;
-    private String questionText;
-    private String answerText;
-
-    private String questionImageUrl;
-    private String questionImagePosition;
-    private String questionImageScale;
-
-    private String answerImageUrl;
-    private String answerImagePosition;
-    private String answerImageScale;
+    private String categoryName;
+    private String bkgColor;
+    private String textColor;
 
     // spring boot apparently needs no-args constructor
     public Category() {
@@ -33,16 +23,10 @@ public class Category {
     // in their respective jeopardies
     // If there is an error writing to the db, check if the names match the table
     // attributes/columns
-
     public String getCategoryId() {
         return categoryId;
     }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getGameId() {
+      public String getGameId() {
         return gameId;
     }
 
@@ -50,84 +34,34 @@ public class Category {
         this.gameId = gameId;
     }
 
-    public String getQnaId() {
-        return qnaId;
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
     }
 
-    public void setQnaId(String qnaId) {
-        this.qnaId = qnaId;
+    public String getCategoryName() {
+        return categoryName;
     }
 
-    public Integer getPtValue() {
-        return ptValue;
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
     }
 
-    public void setPtValue(Integer ptValue) {
-        this.ptValue = ptValue;
+    public String getBkgColor() {
+        return bkgColor;
     }
 
-    public String getQuestionText() {
-        return questionText;
+    public void setBkgColor(String bkgColor) {
+        this.bkgColor = bkgColor;
     }
 
-    public void setQuestionText(String questionText) {
-        this.questionText = questionText;
+    public String getTextColor() {
+        return textColor;
     }
 
-    public String getAnswerText() {
-        return answerText;
+    public void setTextColor(String textColor) {
+        this.textColor = textColor;
     }
 
-    public void setAnswerText(String answerText) {
-        this.answerText = answerText;
-    }
+  
 
-    public String getQuestionImageUrl() {
-        return questionImageUrl;
-    }
-
-    public void setQuestionImageUrl(String questionImageUrl) {
-        this.questionImageUrl = questionImageUrl;
-    }
-
-    public String getQuestionImagePosition() {
-        return questionImagePosition;
-    }
-
-    public void setQuestionImagePosition(String questionImagePosition) {
-        this.questionImagePosition = questionImagePosition;
-    }
-
-    public String getQuestionImageScale() {
-        return questionImageScale;
-    }
-
-    public void setQuestionImageScale(String questionImageScale) {
-        this.questionImageScale = questionImageScale;
-    }
-
-    public String getAnswerImageUrl() {
-        return answerImageUrl;
-    }
-
-    public void setAnswerImageUrl(String answerImageUrl) {
-        this.answerImageUrl = answerImageUrl;
-    }
-
-    public String getAnswerImagePosition() {
-        return answerImagePosition;
-    }
-
-    public void setAnswerImagePosition(String answerImagePosition) {
-        this.answerImagePosition = answerImagePosition;
-    }
-
-    public String getAnswerImageScale() {
-        return answerImageScale;
-    }
-
-    public void setAnswerImageScale(String answerImageScale) {
-        this.answerImageScale = answerImageScale;
-    }
-    
 }
