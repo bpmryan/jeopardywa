5b0a267235680a597606e496b065fe5619fd9261
bpmryan <bpm_ryand2428@yahoo.com>
2025-12-01 21:37:07 -0600

    Create functions that do http post requests in UserController.java

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+
+import model.UserInfo;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // User signup
+    @PostMapping("/signup")
+    public String signup(@RequestBody UserInfo user) {
+        return userService.signup(user, user.getPassword());
+    }
+
+    // User login
+    static class LoginRequest {
+        public String username;
+        public String password;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest req) {
+        String userId = userService.login(req.username, req.password);
+        return (userId == null) ? "ERROR" : userId;
+    }
+}
